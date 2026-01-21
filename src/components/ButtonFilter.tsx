43846512cd3c9eb575e0e4b54086cc44a68ec019
Edu Montalvo <kaedu18@gmail.com>
2026-01-20 21:34:46 -0500

    update challenge

--- src/components/ButtonFilter.tsx
+++ src/components/ButtonFilter.tsx
@@ -5,7 +5,7 @@ type ButtonFilterProps = {
 
 const ButtonFilter = ({name,action} : ButtonFilterProps) => {
     return (
-        <button className="text-white text-xl px-6 py-3 rounded-full bg-dark-700 border border-solid border-dark-600" onClick={action}>
+        <button className="text-white text-xl px-6 py-3 rounded-full bg-dark-700 border border-solid border-dark-600 focus:bg-acent-400 hover:bg-dark-600" onClick={action}>
             {name}
         </button>
     )
